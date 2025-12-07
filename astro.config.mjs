bd3ea2a71055bc4ef13b5867fefea5beabe1d065
Pontus Öberg <pontus.oberg@fellowmind.se>
2025-12-07 10:24:22 +0100

    Add design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com', // Replace with actual domain
+  site: 'https://smarthem.nu',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
